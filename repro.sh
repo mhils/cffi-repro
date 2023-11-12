f5cfab7c8922d3adc10840f9998cad95d741f1a6
Maximilian Hils <git@maximilianhils.com>
2023-11-12 21:00:17 +0100

    ci

--- repro.sh
+++ repro.sh
@@ -5,7 +5,7 @@ brew install python@3.12
 python3.12 -m venv venv
 venv/bin/python3 -m pip install pyopenssl
 
-./run.py
+venv/bin/python3 run.py
 
 venv/bin/python3 -m pip install pyinstaller
-pyinstaller --onefile ./run.py
+venv/bin/pyinstaller --onefile ./run.py
