c51ae969a4e075d96897e814320c6b512382a895
Maximilian Hils <git@maximilianhils.com>
2023-11-12 22:45:33 +0100

    wip

--- repro.sh
+++ repro.sh
@@ -1,11 +1,27 @@
 #!/usr/bin/env bash
 
-brew update
-brew install python@3.12
-python3.12 -m venv venv
-venv/bin/python3 -m pip install pyopenssl
+set -xeou pipefail
 
-venv/bin/python3 run.py
+git clone https://github.com/Homebrew/brew homebrew-x64
 
-venv/bin/python3 -m pip install pyinstaller
-venv/bin/pyinstaller --onedir ./run.py
+arch -x86_64 homebrew-x64/bin/brew install python@3.12
+
+homebrew-x64/bin/python3.12 -m venv venv-x64
+
+venv-x64/bin/pip install pyinstaller pyopenssl
+
+cat >repro.py <<EOL
+from OpenSSL._util import ffi
+print("creating callback")
+ffi.callback(
+	"int (*)(SSL *, unsigned char **, unsigned char *, "
+	"const unsigned char *, unsigned int, void *)",
+	lambda *_, **__: None
+)
+print("ok")
+EOL
+
+venv-x64/bin/pyinstaller --clean --distpath dist-x64 --codesign-identity "Developer ID Application" repro.py
+
+echo "Running x64 version..."
+dist-x64/repro/repro  # hangs
